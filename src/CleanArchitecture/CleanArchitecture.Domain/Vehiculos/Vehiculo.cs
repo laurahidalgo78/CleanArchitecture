5df4f0d60a7af9490ecb585e82bfde6642fe3e8b
Laura Valentina Hidalgo Melo <lauravhidalgom@interrapidisimo.com>
2026-04-07 12:27:11 -0500

    feat: Iniciarlización del proyecto

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
@@ -0,0 +1,11 @@
+namespace CleanArchitecture.Domain.Vehiculos
+{
+    public class Vehiculo
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public string Color { get; set; }
+    }
+}
